d7bd9b1c8943f810aebd2571dc6616348f78e393
thiagofalima <thiagofalima@hotmail.com>
2026-02-27 16:14:17 -0300

    feat(header): implement Header component - Also StyledHeader styled-component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-import './Header.css'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+    width: 100%;
+    padding: 1.5em;
+    background-color: #2D333B;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        font-family: 'Inter', 'Roboto', 'Arial';
+        font-size: 1.5em;
+        color: #FFFFFF;
+    }
+`
 
 export const Header = () => {
     return (
-        <header>
+        <StyledHeader>
             <h1>GitFind</h1>
-        </header>
+        </StyledHeader>
     )
 }
