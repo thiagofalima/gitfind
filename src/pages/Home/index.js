d7fb875a53787ab62d61647892ddd78c741f17dd
thiagofalima <thiagofalima@hotmail.com>
2026-02-27 16:32:31 -0300

    feat: add styled-component Wrapper

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,41 @@
 import { Header } from "../../components/Header";
-import background from "../../assets/background.svg"
+import background from "../../assets/background.svg";
 import styled from "styled-components";
 
-const BackgroundImage = styled.img`
-  height: 80vh;
-`
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-end;
+
+  img.background {
+    height: 100vh;
+    position: absolute;
+    left: 0;
+  }
+
+  .informacoes {
+    width: 60%;
+    margin-right: 90px;
+    margin-top: 40px;
+    color: #ffffff;
+  }
+`;
 
 function App() {
   return (
-  <>
-    <Header />
-    <div className="conteudo">
-      <BackgroundImage src={background} className="background" 
-      alt="background with Github logo icon."/>
-    </div>
-  </>
-);
+    <>
+      <Header />
+      <Wrapper className="conteudo">
+        <img
+          src={background}
+          className="background"
+          alt="background with Github logo icon."
+        />
+        <div className="informacoes">Teste</div>
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
