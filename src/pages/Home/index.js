e0201b437a06c9c7856254a65d2db6fb99cbcf59
thiagofalima <thiagofalima@hotmail.com>
2026-02-27 16:21:29 -0300

    feat: add background image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
 import { Header } from "../../components/Header";
+import background from "../../assets/background.svg"
+import styled from "styled-components";
+
+const BackgroundImage = styled.img`
+  height: 80vh;
+`
 
 function App() {
   return (
   <>
     <Header />
+    <div className="conteudo">
+      <BackgroundImage src={background} className="background" 
+      alt="background with Github logo icon."/>
+    </div>
   </>
 );
 }
